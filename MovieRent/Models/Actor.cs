e08de19c62bc971fe9b7ebab272f3fc20e4bb349
rameen.rizwan01 <rameen.rizwan01@gmail.com>
2023-12-02 15:01:30 +0500

    First

---
+++ MovieRent/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRent.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+
+        public string Name{ get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ MovieRent/Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRent.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int prodId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
