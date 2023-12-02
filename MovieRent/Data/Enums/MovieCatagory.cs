ffb973e3c9dd210f7ca4a08ab3a2e2ee69223abe
rameen.rizwan01 <rameen.rizwan01@gmail.com>
2023-12-02 15:54:46 +0500

    2nd

---
+++ MovieRent/Data/Enums/MovieCatagory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRent.Data
+{
+    public enum MovieCatagory
+    {
+        Action = 1,
+        Drama,
+        Comedy,
+        Documentary
+    }
+}
--- MovieRent/Models/Movie.cs
+++ MovieRent/Models/Movie.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MovieRent.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,26 @@ namespace MovieRent.Models
 {
     public class Movie
     {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Description
+        {
+            get; set;
+        }
+        public string Image
+        {
+            get; set;
+        }
+        public double price{ get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime Enddate { get; set; }
+        public MovieCatagory moviecata { get; set; }
     }
 }
